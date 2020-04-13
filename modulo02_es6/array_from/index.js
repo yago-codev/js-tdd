ee2194e6c0d754fe53c2c0326a5551632b35ca92
yago-codev <yagomilano92@gmail.com>
2020-04-13 16:19:43 -0300

    Array.from() ES6: trasnformando elementos similares a array, em array

---
+++ modulo02_es6/array_from/index.js
@@ -0,0 +1,6 @@
+const text = "Yago";
+
+// o método Array.from irá transformar elementos similares a um array, em array.
+// a sttring "Yago" será transformada em um array: ["Y", "a", "g", "o"],
+// fazendo com que todos os métodos para tratamento de array fiquem acessíveis
+console.log(Array.from(text));
